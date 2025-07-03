85061bb4a0afde922f0d65fdd31e2d32b36f2d05
신성우 <efilevol42@gmail.com>
2025-07-03 15:44:58 +0900

    chore: rune 버전업

--- src/apps/list-view-test/length-test.ts
+++ src/apps/list-view-test/length-test.ts
@@ -28,16 +28,21 @@ class ChatListView extends ListView<ChatItemView> {
 class ChatPage extends Page<{ chats: TChat[] }> {
   private chatListView = new ChatListView(this.data.chats);
 
-  protected onRender() {
+  protected onMount() {
     this.delegate(RemoveButtonClickEvent, ChatItemView, (_, targetView) => {
-      console.log("this.data.chats: ", this.data.chats);
-      this.chatListView.remove(targetView.data);
-      console.log("this.data.chats: ", this.data.chats);
+      console.log("this.data.chats: ", this.data.chats.length);
+
+      this.data.chats.splice(this.data.chats.indexOf(targetView.data), 1);
+
+      console.log("this.data.chats: ", this.data.chats.length);
     });
   }
 
   protected template() {
-    return html`<div>${this.chatListView}</div>`;
+    // return html`<div>${this.chatListView}</div>`;
+    return html`<div>
+      ${this.data.chats.map((chat) => new ChatItemView(chat))}
+    </div>`;
   }
 }
 
